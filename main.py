503eb69677ed8a5f5a5c8e2b9fea1063d8dc6371
Jacob Parmer <jacob.parmer@auburn.edu>
2021-05-01 09:42:27 -0500

    Re-added model work

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from src.model import RNN
 from src.data import LibriSpeech
 from src.time_logs import TimerLog
 
+import torch
+from torch import nn
+
 import argparse
 import os
 import builtins
@@ -18,6 +21,7 @@ import builtins
 _print = print
 stopwatch = TimerLog()
 
+# ------------- HELPER FUNCTIONS --------------- #
 def timed_print(*args):
     print_str = ""
     for arg in args:
@@ -25,7 +29,35 @@ def timed_print(*args):
 
     _print(f"{stopwatch.get_elapsed()}\t| {print_str}")
 
+def get_features(dataset):
 
+    timer = TimerLog()
+
+    if args.verbose:
+        print("Starting Feature Extraction...")
+
+    dataset_size = len(dataset)
+    features = torch.empty(0)
+    for i, data in enumerate(dataset):
+        sig = Signal(data[0], data[1])
+        sig_features = sig.get_mfilterbank().unsqueeze_(-1).transpose(2,0)
+        #sig_features = sig_features.transpose(1,2)
+                        
+        features = torch.cat((features, sig_features), 0)
+
+        if args.verbose:
+            print(f"#{i}/{dataset_size}")
+            sig.print_stats()
+
+        if i == 3:
+            break
+
+    if args.verbose:
+        print(f"Finished Feature Extraction. Total Time Elapsed: {timer.get_elapsed()}")
+
+    return features
+
+# ---------- MAIN PROGRAM EXECUTION ----------- #
 def main(args):
 
     if args.timed:
@@ -43,20 +75,19 @@ def main(args):
                             download=True
                         )
 
-        dataset_size = len(train_clean_100.dataset)
+        features = get_features(train_clean_100.dataset)
+        model = RNN(input_size=len(features[0][0]),
+                    output_size=1,
+                    hidden_dim=12, 
+                    n_layers=1, 
+                    verbose=args.verbose
+                    )
 
-        for i, data in enumerate(train_clean_100.dataset):
-            sig = Signal(data[0], data[1])
+        criterion = nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lrate)
 
-            features = {"MFCC": sig.get_MFCC(),
-                        "pitch": sig.get_pitch()
-                        }
-
-            if args.verbose:
-                print(f"#{i}/{dataset_size}")
-                sig.print_stats()
-            
-
+        for epoch in range(1, args.epochs+1):
+            optimizer.zero_grad()
 
     elif args.mode == "testing":
         if args.verbose:
@@ -70,18 +101,26 @@ def main(args):
                         download=True
                     )
 
-        dataset_size = len(test_clean.dataset)
+        breakpoint()
 
-        for i, data in enumerate(test_clean.dataset):
-            sig = Signal(data[0], data[1])
+        features = get_features(test_clean.dataset)
+        model = RNN(input_size=len(features[0][0]),
+                    output_size=1,
+                    hidden_dim=12,
+                    n_layers=1,
+                    verbose=args.verbose
+                    )
 
-            features = {"MFCC": sig.get_MFCC(),
-                        "pitch": sig.get_pitch()
-                        }
+        criterion = nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lrate)
 
-            if args.verbose:
-                print(f"#{i}/{dataset_size}")
-                sig.print_stats()
+        for epoch in range(1, args.epochs+1):
+            optimizer.zero_grad()
+            features.to(model.device)
+            output, hidden = model(features)
+
+
+
             
     else:
         raise ValueError("Invalid mode selected. Please use CLI parameter '-m training' or '-m testing'.")
@@ -93,6 +132,11 @@ if __name__ == "__main__":
     parser.add_argument("--verbose", "-v", action="store_true", help="Display debugging logs")
     parser.add_argument("--timed", "-t", action="store_true", help="Display execution time in debugging logs")
     parser.add_argument("--mode", "-m",  help="Set program to training or testing mode")
+    parser.add_argument("--lrate", "-l", default=0.01, help="Set learning rate for model")
+    parser.add_argument("--epochs", "-e", default=100, help="Set number of epochs for model training")
     args = parser.parse_args()
 
     main(args)
+
+
+
--- src/model.py
+++ src/model.py
@@ -7,18 +7,43 @@ import os
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
 
 class RNN(nn.Module):
 
-    def __init__(self, verbose=False):
+    def __init__(self, input_size, output_size, hidden_dim, n_layers, verbose=False):
         super(RNN, self).__init__()
 
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
+        self.to(self.device)
         self.verbose = verbose
         if self.verbose:
             print(f'Using {self.device} device')
+        
+
+        # Define params
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+
+        # Define layers
+        self.rnn = nn.RNN(input_size, hidden_dim, n_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_size)
+
+    def forward(self, x):
+        
+        batch_size = x.size(0)
+
+        hidden = self.init_hidden(batch_size)
+
+        out, hidden = self.rnn(x, hidden)
+
+        out = out.contiguous().view(-1, self.hidden_dim)
+        out = self.fc(out)
+
+        return out, hidden
+
+    def init_hidden(self, batch_size):
+        hidden = torch.zeros(self.n_layers, batch_size, self.hidden_dim)
+        return hidden
 
     def export_model(self):
         return
