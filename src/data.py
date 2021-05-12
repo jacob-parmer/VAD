7dd45ddea3e19667af962c7caf16ae855885ab8c
Jacob Parmer <jacob.parmer@auburn.edu>
2021-05-12 12:03:26 -0500

    Added function and object comments

--- src/data.py
+++ src/data.py
@@ -6,21 +6,34 @@
 
 import torch
 from torchaudio import datasets
-from torchvision.transforms import ToTensor, Lambda, Compose
 import os
 import webrtcvad
 import pandas
-import math
 import numpy as np
 
-from src.time_logs import TimerLog
 from src.signals import Signal
 
 FRAME_SIZE_MS = 10
 
 class LibriSpeech:
+    """
+    Handles librispeech data, which includes audio waveforms, sample rate, the text being spoken, and index information.
+    """
 
-    def __init__(self, root, url, folder_in_archive, download=False, batch_size=64, shuffle=True):
+    def __init__(self, root, url, folder_in_archive, download=False):
+        """
+        Creates Librispeech object.
+    
+        If download is set to true, torch's dataset handler will automatically download the .tar to the machine and extract.
+        Note that it doesn't delete the tar after unpacking, so the size ends up being 2x what it needs to be
+
+        Params:
+            (str) root: Path to root directory of the project
+            (str) url: Name of the dataset to be pulled, e.g. "dev-clean", "dev-other".
+                       Can be found here - https://www.openslr.org/12
+            (str) folder_in_archive: Subdirectory to find dataset - should be 'LibriSpeech'.
+            (bool) download: Sets automatic download if files not found            
+        """
         self.dataset = datasets.LIBRISPEECH(
                             root=root,
                             url=url,
@@ -30,22 +43,35 @@ class LibriSpeech:
         self.name = url
         if not os.path.exists(self.dataset._path):
             raise RuntimeError(
-                "Dataset not found. Please use 'download=True' to download it."
+                "Dataset not found. Please use 'download=True' to download it, or manually grab files from https://www.openslr.org/12 and paste them into LibriSpeech/ folder."
             )
 
-        self.labels = []
-
     def load_data(self, index, n_mels, n_mfcc):
+        """
+        Loads features and target values for specified data
 
+        This was added so that data could be passed into the model on a one-by-one basis, rather than loading them all at the beginning and rolling from there.
+        These datasets get pretty big apparently, so loading the features from them all at once causes memory issues.
+
+        I'm pretty sure this function will fail on windows because of the way I'm pulling the file.
+        Either fix this or just dockerize the application so that I don't have to deal with it. :)
+        
+        Params:
+            (int) index: Position in the dataset to load data from
+            (int) n_mels: Number of mel bins to return from MFCC feature extraction
+            (int) n_mfcc: Number of cepstral coefficients to return from MFCC feature extraction
+
+        Returns:
+            (tensor) X: MFCC data of shape [?,?,?]
+            (tensor) y: Label data of shape [?,?,?]
+        """
         # 1. Get MFCC features from data at index
         data = Signal(self.dataset[index][0], self.dataset[index][1])
         frame_size = int(data.sample_rate * (FRAME_SIZE_MS / 1000.0))
-
-        data.split_into_frames(frame_size=frame_size) # webrtc only supports 10, 20, 30 ms frames
         X = data.get_MFCC(hop_length=frame_size, n_mels=n_mels, n_mfcc=n_mfcc).transpose(2,0).transpose(1,2)
 
         # 2. Get labels for each frame in MFCC features
-        label_dir = label_dir = os.getcwd() + "/LibriSpeech/labels/" + self.name # I think this will break on windows? Checking needed.
+        label_dir = label_dir = os.getcwd() + "/LibriSpeech/labels/" + self.name 
         file_dir = label_dir + "/" + str(self.dataset[index][3]) + "/" + str(self.dataset[index][4])
         file_name = file_dir + f"/{self.dataset[index][3]}-{self.dataset[index][4]}-{str(self.dataset[index][5]).zfill(4)}.csv"
 
@@ -55,6 +81,19 @@ class LibriSpeech:
 
     # -------- PRIVATE MEMBERS ----------- #
     def _label_data(self, dataset_name, verbose=False):
+        """
+        Creates labels and saves them as .csv files to LibriSpeech/labels/ directory.
+
+        Utilizes webrtcvad package to generate labels, currently set to mode 3, which aggressively filters out non-speech.
+        Could possibly lead to higher FRR.
+
+        Params:
+            (str) dataset_name: Name of the dataset to be labelled, e.g. "dev-clean".
+            (bool) verbose: Displays running information to CLI while running if this is enabled
+
+        Returns:
+            .csv files in LibriSpeech/labels/dataset_name subdirectory
+        """
         if verbose:
             print("Beginning VAD labeling...")
             print("-------------------------")
@@ -66,9 +105,9 @@ class LibriSpeech:
         for i, data in enumerate(self.dataset):
             sig = Signal(data[0], data[1])
 
-            sig.split_into_frames(frame_size=int(sig.sample_rate * (FRAME_SIZE_MS / 1000.0))) # webrtc only supports 10, 20, 30 ms frames
+            split_waveform = sig.split_into_frames(frame_size=int(sig.sample_rate * (FRAME_SIZE_MS / 1000.0))) # webrtc only supports 10, 20, 30 ms frames
 
-            labels = [1 if vad.is_speech(np.int16(f * 32768).tobytes(), sample_rate=sig.sample_rate) else 0 for f in sig.split_waveform]
+            labels = [1 if vad.is_speech(np.int16(f * 32768).tobytes(), sample_rate=sig.sample_rate) else 0 for f in split_waveform]
 
             # Write labels to .csv files
             file_label_dir = label_dir + "/" + str(self.dataset[i][3]) + "/" + str(self.dataset[i][4])
@@ -91,14 +130,24 @@ class LibriSpeech:
 
 
 def build_librispeech(mode, verbose=False):
+    """
+    Creates and returns librispeech objects for each dataset in datasets variable.
+    Also creates labels if they don't already exist.
 
+    Params:
+        (str) mode: 'training' or 'testing', however the program is being run
+        (bool) verbose: Displays running information to CLI while running if this is enabled
+
+    Returns:
+        (dict) librispeech: Contains all the data from each of the loaded datasets
+    """
     if verbose:
         print("Loading LibriSpeech Data...")
         print("If LibriSpeech is not already downloaded, this might take a while...")
 
     librispeech = {}
     if mode == 'training':
-        datasets = ["dev-clean", "dev-other", "train-clean-100", "train-clean-360"]
+        datasets = ["train-other-500", "train-clean-360"] # Extras to add? "train-clean-100", "dev-clean", "dev-other"
     elif mode == 'testing':
         datasets = ["test-clean", "test-other"]
     else:
